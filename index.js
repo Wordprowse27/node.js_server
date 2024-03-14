83c030cc6447f72a62fd3164934108ace6cc45bf
leonbarigye <deconpromise@gmail.com>
2024-03-14 19:34:25 +0300

    add modules folder that contains database a json file add route to get the json file data

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ import express from 'express';
 import morgan from 'morgan';
 import path from 'path';
 import { fileURLToPath } from 'url';
+import fs from 'fs';
 
 
 
@@ -23,11 +24,45 @@ app.use(express.json())
 app.use(express.urlencoded({extended: false}))
 app.use(morgan('dev'));
 app.use(express.static('frontend'))
-
+TODO: "Work on the posts"
+app.post("/api/courses",(req, res)=>{
+    // add data to be added to the database.
+})
 
 app.get('/login',(req, res)=>{
     res.sendFile(path.join(__dirname, 'frontend','login.html'))
 })
+TODO: "CONNECT TO DATABASE"
+
+app.get('/api/courses', (req, res)=>{
+    // using asynchronous readfile
+    fs.readFile( './modules/database.json', "utf-8", (error, datastring)=>{
+        if (error){
+            console.error('Error reading file:', error);
+            res.status(500).send('Internal Server Error');
+            return;
+        } 
+        try{
+            // convert a sting to a JSON object
+            const info = JSON.parse(datastring);
+            // Send the JSON data as a response
+            res.json(info);
+            }
+            // If it throws an error console log the error.
+            catch (error){
+            console.error('Error parsing JSON data:', error);
+            res.status(500).send('Internal Server Error');
+            }
+        })
+})
+TODO: "get by id"
+// app.get("/api/courses/:id",(req, res)=>{
+
+// })
+
+// app.patch("/api/:id",(req, res)=>{
+
+// })
 
 app.post('/login',(req, res)=>{
     const { username, password } = req.body;
